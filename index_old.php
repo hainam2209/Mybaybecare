e567b4604932f936d4dce6a61735128a83fa50ba
hainam2209 <levannam2209@gmail.com>
2018-06-23 23:16:49 +0700

    fad
    ghi chú mới

---
+++ index_old.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <title>BabeCare.com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/myStyle.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<img src="images/banner/mother_12.jpg" alt="Quà tặng mẹ" height="80px" />
+	</div>
+	<div id="container_header" class="container">
+		<div class="row">
+			<div class="col-sm-2">
+				<a id="logo_header" href="index.php"></a>
+			</div>
+			<div class="col-sm-5"></div>
+			<div class="col-sm-2"></div>
+			<div class="col-sm-1"></div>
+			<div class="col-sm-1"></div>
+			<div class="col-sm-1"></div>
+		</div>
+		
+	</div>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>BabyCare.com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="css/myStyle.css">
+  
+  <script src="js/jquery_1.12.2.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+  
+  
+  
+    
+      
+        <ul class="breadcrumb">
+          <li><a href="#">Trang chủ</a></li>
+          <li><a href="#">Đồ chơi cho trẻ sơ sinh</a></li>
+          <li class="active">Ty giả, ty ngậm</li>
+        </ul>
+      
+
+    
+  
+  
+</body>
+</html>
